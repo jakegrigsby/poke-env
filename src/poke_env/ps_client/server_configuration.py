1ab9c2a9519cb6280971fe0704599e8de983a8dc
Jake Grigsby <grigsby@cs.utexas.edu>
2025-01-13 02:18:48 +0000

    update showdown server config

--- src/poke_env/ps_client/server_configuration.py
+++ src/poke_env/ps_client/server_configuration.py
@@ -1,5 +1,3 @@
-"""This module contains objects related to server configuration.
-"""
 from typing import NamedTuple
 
 
@@ -7,16 +5,17 @@ class ServerConfiguration(NamedTuple):
     """Server configuration object. Represented with a tuple with two entries: server url
     and authentication endpoint url."""
 
-    server_url: str
+    websocket_url: str
     authentication_url: str
 
 
 LocalhostServerConfiguration = ServerConfiguration(
-    "localhost:8000", "https://play.pokemonshowdown.com/action.php?"
+    "ws://localhost:8000/showdown/websocket",
+    "https://play.pokemonshowdown.com/action.php?",
 )
 """Server configuration with localhost and smogon's authentication endpoint."""
 
 ShowdownServerConfiguration = ServerConfiguration(
-    "sim.smogon.com:8000", "https://play.pokemonshowdown.com/action.php?"
+    "wss://sim3.psim.us/showdown/websocket",
+    "https://play.pokemonshowdown.com/action.php?",
 )
-"""Server configuration with smogon's server and authentication endpoint."""
